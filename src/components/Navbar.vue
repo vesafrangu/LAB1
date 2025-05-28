a260ab4424fa3cef5c0c5d9bb73a6efe4fffdcc8
vesafrangu <vf70665@ubt-uni.net>
2025-05-28 14:46:54 +0200

    Update Navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,19 +1,35 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-light bg-white shadow-sm w-100">
+  <nav class="navbar navbar-expand-lg shadow-sm w-100" :style="navbarStyle">
     <div class="container">
-      <a class="navbar-brand d-flex align-items-center" href="#">
+      <router-link to="/" class="navbar-brand d-flex align-items-center">
         <img :src="logo" alt="Travelnest Logo" height="40" class="me-2" />
-        <span class="fw-bold fs-4 text-primary">Travelnest</span>
-      </a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+        <span class="fw-bold fs-4 text-light">Travelnest</span>
+      </router-link>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+      >
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav ms-auto">
-          <li class="nav-item"><a class="nav-link text-primary" href="#">Home</a></li>
-          <li class="nav-item"><a class="nav-link text-primary" href="#">Countries</a></li>
-          <li class="nav-item">  <router-link to="/contact" class="nav-link text-primary">Contact</router-link>
-</li>
+          <li class="nav-item">
+            <router-link to="/login" class="nav-link text-light">Login</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/register" class="nav-link text-light">Register</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/about" class="nav-link text-light">About Us</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/services" class="nav-link text-light">Our Services</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/contact" class="nav-link text-light">Contact Us</router-link>
+          </li>
         </ul>
       </div>
     </div>
@@ -21,12 +37,29 @@
 </template>
 
 <script>
+import logoImg from '@/assets/Logoo.png';
+
 export default {
-  name: 'Navbar',
+  name: 'AppNavbar',
   data() {
     return {
-      logo: new URL('@/assets/Logoo.png', import.meta.url).href,
+      logo: logoImg,
+      navbarStyle: {
+        backgroundColor: '#4a4a4a',
+        borderBottom: '2px solid #393e46',
+      },
     };
   },
 };
 </script>
+
+<style scoped>
+.navbar-toggler-icon {
+  filter: invert(1);
+}
+
+.navbar-nav .nav-link:hover {
+  color: #f0a500;
+  text-decoration: underline;
+}
+</style>
