5b484fc535ed578822c520a1d677ce257f8d5020
vesafrangu <vf70665@ubt-uni.net>
2025-05-28 14:16:45 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,32 @@
 <template>
-  <router-view />
+  <div id="app" class="d-flex flex-column min-vh-100">
+    <AppNavbar />
+    <router-view />
+    <AppFooter />
+  </div>
 </template>
 
 <script>
+import AppNavbar from './components/Navbar.vue';
+import AppFooter from './components/Footer.vue';
+
 export default {
   name: 'App',
+  components: {
+    AppNavbar,
+    AppFooter,
+  },
 };
 </script>
+
+<style>
+#app {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+router-view {
+  flex-grow: 1;
+}
+</style>
